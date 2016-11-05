453ec6aefa818f18063296c44ffb8fd291c12465
Kevin Paulus <goarilla@gmail.com>
2016-11-05 22:43:22 +0100

    new script: spie on pastebin.ca newest posts

--- try.sh
+++ try.sh
@@ -1,36 +1,18 @@
-#! /bin/sh
-# vim: set ts=4 sts=4 sw=4 expandtab smartindent tw=78:
-# script that speaks irc chat logs with Mac OS X's say utility.
+ #! /bin/sh
+NAP=60
+old=0
 
-# say(1) voices
-# Agnes               en_US    # Isn't it nice to have a computer that will talk to you?
-# Albert              en_US    #  I have a frog in my throat. No, I mean a real frog!
+while [ true ]; do 
+	rm newest.php
+	wget 'http://en.pastebin.ca/newest.php'
+	
+        id="$(grep -m 1 'http://en.pastebin.ca/[0-9][0-9]*' newest.php)"
+	id="$(echo "$id" | sed -e 's/..*pastebin.ca\/\([0-9][0-9]*\).*/\1/g')"
 
-VOICES="$(say -v ? | grep 'en_')"
-VOICELEN="$(echo "$VOICES" | wc -l)"
+        if [ x"$old" != x"$id" ]; then
+        	open 'http://en.pastebin.ca/newest.php'
+                old="$id"
+        fi
 
-get_val()
-{
-	# return numeric value of char in alphabet
-	python -c "print ord('$1')"
-}
-
-score_speaker()
-{
-	# score between 1-30 (VOICELEN of 'en_' voices)
-	score=0
-	for char in $(echo "$1" | sed -e 's/./\ &/g'); do
-		score=$(( score+$(get_val $char) ))
-	done
-	echo $(( $((score%VOICELEN))+1 ))
-
-}
-
-while read line;
-do
-  echo "$line" 
-	speaker="$(echo "$line" | cut -d':' -f1)"
-	msg="$(echo "$line" | cut -d':' -f2-)"
-  
-	say -v "$(echo "$VOICES" | sed -n -e "$(score_speaker "$speaker")"p | cut -d' ' -f1)" "$msg"
-done < "$1"
+	sleep $(( $(($((NAP*3))/4)) + $((RANDOM%$(($NAP/2)))) ))
+done 
